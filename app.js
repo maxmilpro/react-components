2ca9f423e761ba1c69a78379d44e89f22043cfab
Max Proano <proanom89@gmail.com>
2020-11-30 20:48:34 -0800

    Complete React Components

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // TODO
+
+var groceries = ['Milk', 'Bread', 'Cheese'];
+
+var GroceryList = () => (
+  <ul>
+    {groceries.map((grocery) =>
+      <GroceryListItem item={grocery} />
+    )}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render () {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>
+    )
+  }
+}
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
